abf7ab2a2ba5cc9a92e85abb28c1d36638da6005
kiran.mehetre <mehetrekiran2@gmail.com>
2017-12-27 11:58:41 +0530

    add

--- issue.module.ts
+++ issue.module.ts
@@ -5,16 +5,28 @@ import { Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { IssueTrackerComponent } from './issue-tracker.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { BlogComponent } from './blog/blog.component';
 
 const issueRouting: Routes = [
   {
       path: '',
       component: IssueTrackerComponent
-  }
+  },
+  {
+    path: 'Gallary',
+    component: GallaryComponent
+},
+{
+  path: 'Blog',
+  component: BlogComponent
+}
 ]
 @NgModule({
   declarations: [
-    IssueTrackerComponent
+    IssueTrackerComponent,
+    GallaryComponent,
+    BlogComponent
   ],
   imports: [
     ReactiveFormsModule,
